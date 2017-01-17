56b86ab4d1e800b9f9f16af6cf76f208faa5c464
Kok Fong Tan <kokfong.tan@nintex.com>
2017-01-17 17:30:25 +0800

    Tour of Heroes (up to chapter 6)

---
+++ app/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: "my-heroes",
+  providers: [HeroService],
+  templateUrl: 'app/heroes.component.html',
+  styleUrls: ['app/heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService: HeroService, private router: Router) { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
